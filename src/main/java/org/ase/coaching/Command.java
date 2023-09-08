11a5612dac0536e9cd8f06171f1a212bb0c6d755
Manuel <mantel1337@gmail.com>
2023-09-08 15:00:59 +0200

    expand commands and validation

--- src/main/java/org/ase/coaching/Command.java
+++ src/main/java/org/ase/coaching/Command.java
@@ -1,32 +1,63 @@
 package org.ase.coaching;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Command {
+    private static final List<String> VALIDCOLUMNS = List.of("A", "B", "C");
+    private static final List<String> VALIDROWS = List.of("0", "1", "2");
+    private static final List<String> VALID_GAME_COMMANDS = List.of("end", "start");
     private final String command;
-
-    List<String> validColumns = List.of("A", "B", "C");
-    List<String> validRows = List.of("0", "1", "2");
 
     public Command(String command) {
         this.command = command;
     }
 
+    public static List<Command> getMovingCommands() {
+        return VALIDCOLUMNS.stream()
+                .flatMap(column -> VALIDROWS.stream().map(row -> new Command(column + row)))
+                .toList();
+    }
+
     public boolean isValid() {
         if (command.length() != 2) {
-            return false;
+            return VALID_GAME_COMMANDS.contains(command);
         }
         var firstLetter = command.substring(0, 1);
         var secondLetter = command.substring(1, 2);
 
-        return validColumns.contains(firstLetter) && validRows.contains(secondLetter);
+        return VALIDCOLUMNS.contains(firstLetter) && VALIDROWS.contains(secondLetter);
     }
 
     public int getColumn() {
-        return validColumns.indexOf(command.substring(0, 1));
+        return VALIDCOLUMNS.indexOf(command.substring(0, 1));
     }
 
     public int getRow() {
-        return validRows.indexOf(command.substring(1, 2));
+        return VALIDROWS.indexOf(command.substring(1, 2));
+    }
+
+    @Override
+    public String toString() {
+        return "Command{" +
+                "command='" + command + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Command command1 = (Command) o;
+        return Objects.equals(command, command1.command);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(command);
     }
 }
--- src/test/java/org/ase/coaching/CommandTest.java
+++ src/test/java/org/ase/coaching/CommandTest.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CommandTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {"A0", "A1", "A2", "B0", "B1", "B2", "C0", "C1", "C2"})
+    @ValueSource(strings = {"A0", "A1", "A2", "B0", "B1", "B2", "C0", "C1", "C2", "end", "start"})
     void valid(String command) {
         Command sut = new Command(command);
 
@@ -36,5 +38,21 @@ class CommandTest {
         Command sut = new Command("B2");
 
         assertThat(sut.getRow()).isEqualTo(2);
+    }
+
+    @Test
+    void getMovingCommands() {
+        List<Command> sut = Command.getMovingCommands();
+
+        assertThat(sut).containsExactlyInAnyOrder(
+                new Command("A0"),
+                new Command("A1"),
+                new Command("A2"),
+                new Command("B0"),
+                new Command("B1"),
+                new Command("B2"),
+                new Command("C0"),
+                new Command("C1"),
+                new Command("C2"));
     }
 }
