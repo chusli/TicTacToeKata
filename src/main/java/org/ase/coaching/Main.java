9aae70c4e6127d4ecbcdf46097c12daf41d85dfe
Manuel <mantel1337@gmail.com>
2023-09-08 14:22:21 +0200

    refactor main for interpreting commands

--- src/main/java/org/ase/coaching/Main.java
+++ src/main/java/org/ase/coaching/Main.java
@@ -6,17 +6,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Game game = new Game();
-        Scanner in = new Scanner(System.in);
+
 
         Player player = Player.X;
         do {
             System.out.println(game.getState());
-            var command = new Command(in.nextLine());
+            var command = readCommand();
 
             game.makeMove(player, command);
             player = player == Player.X ? Player.O : Player.X;
 
         } while (1 == 1);
+    }
+
+    private static Command readCommand() {
+        Scanner in = new Scanner(System.in);
+        Command command;
+        do {
+            command = new Command(in.nextLine());
+        } while (command == null || !command.isValid());
+        return command;
     }
 
 }
